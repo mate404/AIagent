51582e098e40a31c2a4d986c28c936e91202effd
mate404 <58915082+mate404@users.noreply.github.com>
2025-12-02 22:37:57 +0100

    can change if it shows tokens

--- AIagent/AIagent/main.py
+++ AIagent/AIagent/main.py
@@ -1,30 +1,44 @@
-import os
 import argparse
+import os
 
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+
 def main():
+    parser = argparse.ArgumentParser(description="AI Code Assistant")
+    parser.add_argument("user_prompt", type=str, help="Prompt to send to Gemini")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
         raise RuntimeError("GEMINI_API_KEY environment variable not set")
 
     client = genai.Client(api_key="AIzaSyBwkNNj9EhkM1wBBWuRMHWZFHWipb7cnv8")
-    parser = argparse.ArgumentParser(description="Chatbot")
-    parser.add_argument("prompt", type=str, help="User prompt")
-    args = parser.parse_args()
-    messages = [types.Content(role="user", parts=[types.Part(text=args.prompt)])]
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}\n")
+
+    generate_content(client, messages, args.verbose)
+
+
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=messages,
     )
-    
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")
+
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
     print("Response:")
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens:{response.usage_metadata.candidates_token_count}")
+
 
 if __name__ == "__main__":
     main()
